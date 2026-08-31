9572e181403f56cac024d1eaa51acb8e77f36a93
baodingfengyun <liupu.mail@gmail.com>
2026-08-31 15:45:17 +0800

    同步

--- Assets/Scripts/Game/LocalizeResources/LocalizeResources.cs
+++ Assets/Scripts/Game/LocalizeResources/LocalizeResources.cs
@@ -1,17 +1,26 @@
 ﻿using static FrameBaseDefine;
 using static GB;
 
+/// <summary>
+/// 本地化资源
+/// </summary>
 public class LocalizeResources
 {
+	// 简体中文
 	public string mChinese;
+	// 英文
 	public string mEnglish;
+	// 繁体中文
 	public string mChineseTraditional;
+
 	public LocalizeResources(string chinese, string english, string chineseTraditional)
 	{
 		mChinese = chinese;
 		mEnglish = english;
 		mChineseTraditional = chineseTraditional;
 	}
+
+	// 获取本地化语言，不匹配的话就默认简体中文
 	public string getCurLocalization()
 	{
 		string curLanguage = mLocalizeResourcesManager.getCurLanguage();
